f55cc3fb2bdce0fe2592a6f8e4304e2b80e191c9
Rux-Lsr <russelsonwa1@gmail.com>
2024-08-15 17:33:16 +0100

    suppresion du supperflu dans les favoris

--- lib/page/favoris.dart
+++ lib/page/favoris.dart
@@ -4,33 +4,13 @@ import 'package:google_fonts/google_fonts.dart';
 class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFFEBF3EB),
-      appBar: AppBar(
-        backgroundColor: Color(0xFF299417),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.white),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        title: Text(
-          'Favoris',
-          style: GoogleFonts.inter(
-            fontWeight: FontWeight.w700,
-            fontSize: 16,
-            color: Colors.white,
-          ),
-        ),
-      ),
-      body: Center(
-        child: Text(
-          'Aucun favoris',
-          style: GoogleFonts.inter(
-            fontWeight: FontWeight.w200,
-            fontSize: 16,
-            color: Color(0xFF000000),
-          ),
+    return Center(
+      child: Text(
+        'Aucun favoris',
+        style: GoogleFonts.inter(
+          fontWeight: FontWeight.w200,
+          fontSize: 16,
+          color: Color(0xFF000000),
         ),
       ),
     );

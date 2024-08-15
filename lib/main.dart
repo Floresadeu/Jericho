f8960c64a869aaab0eb1ecc6eeb33aaf20a74ffd
Rux-Lsr <russelsonwa1@gmail.com>
2024-08-15 17:29:02 +0100

    splash screen propre

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
-import "package:sqflite/sqlite_api.dart";
-import 'page/plache_holder.dart';
+import 'package:jericho_plays/page/acceuil.dart';
 
 Future<void> main() async {
-
-  WidgetsFlutterBinding.ensureInitialized();
-
-final Future<Database> database = openDatabase(
-
-  join(await getDatabasesPath(),
-   'jericho_play_database.db'),
-
-  onCreate: (db, version) {
-    // Run the CREATE TABLE statement on the database.
-    return db.execute(
-      'CREATE TABLE Chant(id INTEGER PRIMARY KEY, numPage int ,title TEXT, parole  Texte, voix1 Texte, voix2 Texte, voix3 Texte,voix4 Texte)',
-    );
-  },
-  version: 1,
-);
-   
-
-
-  runApp( MyApp(database: database));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final Future<Database> database;
-  const MyApp({Key? Key, required this.database}) : super(key: Key);
-
-
-
-
-
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: SplashScreen(),
+      home: const HomePage(
+        paroles: "Test de chant",
+      ),
     );
   }
 }
